e809081a9bcf37a0d0108c4e1bb19f89efa1f588
Lukas Arnold <l.arnold@fz-juelich.de>
2013-07-24 18:20:52 +0200

    no message

--- createfds.py
+++ createfds.py
@@ -1,6 +1,18 @@
 import sys
 import xml.etree.ElementTree as ET
 
+def div235(n):
+    r_init = int(n)
+    while True:
+        r = r_init
+        while r%2 == 0 and r!= 0: r /= 2
+        while r%3 == 0 and r!= 0: r /= 3
+        while r%5 == 0 and r!= 0: r /= 5
+
+        if r == 1: break
+        r_init += 1
+    return r_init
+
 def obst(node, vars):
     print "OBST XB=%f,%f,%f,%f,%f,%f"%(eval(node.attrib["x1"], {}, vars),
                                        eval(node.attrib["x2"], {}, vars),
@@ -11,7 +23,8 @@ def obst(node, vars):
 
 def var(node, vars):
     for att in node.attrib:
-        vars[att] = eval(node.attrib[att], {}, vars)
+        vars[att] = eval(node.attrib[att], globals(), vars)
+        print "added variable: %s = %f"%(att, vars[att])
 
 def loop(node, vars):
     start = int(node.attrib['start'])
@@ -31,7 +44,7 @@ root = tree.getroot()
 vars = {}
 
 for node in root:
-    print node
+    #print node
     
     if node.tag == 'obst': obst(node, vars)
     if node.tag == 'var': var(node, vars)
