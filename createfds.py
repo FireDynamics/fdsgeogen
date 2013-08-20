a4abffe0402b100ce3b2b4035f2621540e8879a5
Lukas Arnold <l.arnold@fz-juelich.de>
2013-08-20 15:02:34 +0200

    added new features...

--- createfds.py
+++ createfds.py
@@ -14,34 +14,51 @@ def div235(n):
         r_init += 1
     return r_init
 
+def add_var(key, value):
+    global vars
+    vars[key] = value
+    
+def del_var(key):
+    global vars
+    del vars[key]
+
+def check_val(node, lst, req=False):
+    for item in lst:
+        if not item in node.attrib: 
+            if req:
+                print "required attribute %s not found in node %s"%(item, node)
+                sys.exit()
+            return False
+    return True 
+
 def get_val(node, name, opt=False):
     if name in node.attrib:
-        return eval(node.attrib["nx"], {}, vars)
+        return eval(node.attrib[name], {}, vars)
     elif (opt) and (name in vars):
         return vars[name]
     else:
         print "error reading attribute %s from node %s"%(name, node)
         sys.exit()
 
-def write_to_fds(vars, text):
+def write_to_fds(text):
     if type(vars['fds_file']) != file:
-        open_fds_file(vars)
+        open_fds_file()
         
     vars['fds_file'].write(text)
 
-def close_fds_file(vars):
-    write_to_fds(vars, "&TAIL/\n")
+def close_fds_file():
+    write_to_fds("&TAIL/\n")
     vars['fds_file'].close()
     
-def open_fds_file(vars):
+def open_fds_file():
     if type(vars['fds_file']) == file:
-        close_fds_file(vars)
+        close_fds_file()
         
     vars['fds_file'] = open(vars['outfile'], 'w')
-    write_to_fds(vars, "&HEAD CHID='%s', TITLE='%s' /\n"%(vars['chid'], vars['title']))
+    write_to_fds("&HEAD CHID='%s', TITLE='%s' /\n"%(vars['chid'], vars['title']))
 
 
-def info(node, vars):
+def info(node):
 
     if 'chid' in node.attrib: 
         vars['chid'] = eval(node.attrib["chid"], {}, vars)
@@ -50,9 +67,9 @@ def info(node, vars):
     if 'outfile' in node.attrib: 
         vars['outfile'] = eval(node.attrib["outfile"], {}, vars)
         print "outfile: %s"%vars['outfile']
-        open_fds_file(vars)
+        open_fds_file()
         
-def obst(node, vars):
+def obst(node):
     geometry = "XB=%f,%f,%f,%f,%f,%f"%(eval(node.attrib["x1"], {}, vars),
                                        eval(node.attrib["x2"], {}, vars),
                                        eval(node.attrib["y1"], {}, vars),
@@ -62,17 +79,17 @@ def obst(node, vars):
     color = ""
     if 'color' in node.attrib:
         color = "COLOR='%s'"%node.attrib["color"]
-    write_to_fds(vars,"&OBST %s %s/\n"%(geometry, color))
+    write_to_fds("&OBST %s %s/\n"%(geometry, color))
     
-def hole(node, vars):
-    write_to_fds(vars,"&HOLE XB=%f,%f,%f,%f,%f,%f /\n"%(eval(node.attrib["x1"], {}, vars),
+def hole(node):
+    write_to_fds("&HOLE XB=%f,%f,%f,%f,%f,%f /\n"%(eval(node.attrib["x1"], {}, vars),
                                                         eval(node.attrib["x2"], {}, vars),
                                                         eval(node.attrib["y1"], {}, vars),
                                                         eval(node.attrib["y2"], {}, vars),
                                                         eval(node.attrib["z1"], {}, vars),
                                                         eval(node.attrib["z2"], {}, vars)))
                                                         
-def mesh(node, vars):
+def mesh(node):
     nmeshes = 1
     px = 1
     py = 1
@@ -116,57 +133,65 @@ def mesh(node, vars):
                 zmin = gzmin + iz*dz
                 zmax = gzmin + (iz+1)*dz                
                 
-                write_to_fds(vars,"&MESH IJK=%d,%d,%d, XB=%f,%f,%f,%f,%f,%f /\n"%(
+                write_to_fds("&MESH IJK=%d,%d,%d, XB=%f,%f,%f,%f,%f,%f /\n"%(
                              lnx, lny, lnz,
                              xmin, xmax, ymin, ymax, zmin, zmax))
 
-def boundary(node, vars):
+def boundary(node):
     if 'x' in node.attrib: 
         if node.attrib['x'] == "open":
-            write_to_fds(vars, "&VENT MB='XMIN' ,SURF_ID='OPEN' /\n")
-            write_to_fds(vars, "&VENT MB='XMAX' ,SURF_ID='OPEN' /\n")            
+            write_to_fds("&VENT MB='XMIN' ,SURF_ID='OPEN' /\n")
+            write_to_fds("&VENT MB='XMAX' ,SURF_ID='OPEN' /\n")            
 
     if 'y' in node.attrib: 
         if node.attrib['x'] == "open":
-            write_to_fds(vars, "&VENT MB='YMIN' ,SURF_ID='OPEN' /\n")
-            write_to_fds(vars, "&VENT MB='YMAX' ,SURF_ID='OPEN' /\n")            
+            write_to_fds("&VENT MB='YMIN' ,SURF_ID='OPEN' /\n")
+            write_to_fds("&VENT MB='YMAX' ,SURF_ID='OPEN' /\n")            
 
     if 'z' in node.attrib: 
         if node.attrib['z'] == "open":
-            write_to_fds(vars, "&VENT MB='ZMIN' ,SURF_ID='OPEN' /\n")
-            write_to_fds(vars, "&VENT MB='ZMAX' ,SURF_ID='OPEN' /\n")  
+            write_to_fds("&VENT MB='ZMIN' ,SURF_ID='OPEN' /\n")
+            write_to_fds("&VENT MB='ZMAX' ,SURF_ID='OPEN' /\n")  
     if 'zmin' in node.attrib: 
         if node.attrib['zmin'] == "open":
-            write_to_fds(vars, "&VENT MB='ZMIN' ,SURF_ID='OPEN' /\n")
+            write_to_fds("&VENT MB='ZMIN' ,SURF_ID='OPEN' /\n")
     if 'zmax' in node.attrib: 
         if node.attrib['zmax'] == "open":
-            write_to_fds(vars, "&VENT MB='ZMAX' ,SURF_ID='OPEN' /\n")  
+            write_to_fds("&VENT MB='ZMAX' ,SURF_ID='OPEN' /\n")  
 
 
-def input(node, vars):
+def input(node):
     if 'text' in node.attrib: 
-        write_to_fds(vars,"&%s /\n"%(node.attrib["text"]))
+        write_to_fds("&%s /\n"%(node.attrib["text"]))
     if 'str' in node.attrib: 
-        write_to_fds(vars,"&%s /\n"%(eval(node.attrib["text"], {}, vars)))
+        write_to_fds("&%s /\n"%(eval(node.attrib["text"], {}, vars)))
 
-def var(node, vars):
+def var(node):
+    global vars
     for att in node.attrib:
         vars[att] = eval(node.attrib[att], globals(), vars)
-        print "added variable: %s = %f"%(att, vars[att])
+        print "added variable: %s = %s"%(att, str(vars[att]))
 
-def loop(node, vars):
-    start = int(node.attrib['start'])
-    stop  = int(node.attrib['stop'])
-    
-    for loop_i in range(start, stop):
-        local_vars = dict(vars.items() + {node.attrib['var'] : loop_i}.items())
-    
-        for local_node in node:
-            if local_node.tag == 'obst': obst(local_node, local_vars)
-            if local_node.tag == 'var': var(local_node, local_vars)
-            if local_node.tag == 'loop': loop(local_node, local_vars)
+def loop(node):
 
-def fire(node, vars):
+    # integer iteration from start to stop
+    if 'start' in node.attrib and 'stop' in node.attrib:
+        start = int(get_val(node,'start'))
+        stop  = int(get_val(node,'stop'))
+    
+        for loop_i in range(start, stop):
+            add_var(node.attrib['var'], loop_i)
+            traverse(node)
+            del_var(node.attrib['var'])
+            
+    if 'list' in node.attrib:
+        llist = node.attrib['list'].split(',')
+        for loop_i in llist:
+            add_var(node.attrib['var'], loop_i.strip())
+            traverse(node)
+            del_var(node.attrib['var'])
+
+def fire(node):
     if node.attrib['type'] == "burningbox":
         cx = eval(node.attrib['cx'], {}, vars)
         cy = eval(node.attrib['cy'], {}, vars)        
@@ -176,27 +201,27 @@ def fire(node, vars):
         box_obst  = "&OBST "
         box_obst += "XB=%f, %f, %f, %f, %f, %f"%(cx-w2, cx+w2, cy-w2, cy+w2, lz, lz+h)
         box_obst += "/\n"
-        write_to_fds(vars, box_obst)
+        write_to_fds(box_obst)
         
-        write_to_fds(vars, "&REAC FUEL = 'METHANE' /\n")
+        write_to_fds("&REAC FUEL = 'METHANE' /\n")
         hrrpua = eval(node.attrib['hrr'], {}, vars) / (2.0*w2)**2
-        write_to_fds(vars, "&SURF ID='burningbox', HRRPUA=%f /\n"%hrrpua)
-        write_to_fds(vars, "&VENT XB=%f,%f,%f,%f,%f,%f SURF_ID='burningbox' color='RED'/\n"%(cx-w2, cx+w2, cy-w2, cy+w2, lz+h, lz+h))
+        write_to_fds("&SURF ID='burningbox', HRRPUA=%f /\n"%hrrpua)
+        write_to_fds("&VENT XB=%f,%f,%f,%f,%f,%f SURF_ID='burningbox' color='RED'/\n"%(cx-w2, cx+w2, cy-w2, cy+w2, lz+h, lz+h))
 
-def slice(node, vars):
+def slice(node):
     q = "QUANTITY='%s'"%node.attrib['q']
     v = ""
     if 'v' in node.attrib:
         if node.attrib['v'] == '1': v="VECTOR=.TRUE."
     if 'x' in node.attrib:
         pos = eval(node.attrib['x'], {}, vars)
-        write_to_fds(vars, "&SLCF PBX=%e, %s %s /\n"%(pos, q, v))
+        write_to_fds("&SLCF PBX=%e, %s %s /\n"%(pos, q, v))
     if 'y' in node.attrib:
         pos = eval(node.attrib['y'], {}, vars)
-        write_to_fds(vars, "&SLCF PBY=%e, %s %s /\n"%(pos, q, v))
+        write_to_fds("&SLCF PBY=%e, %s %s /\n"%(pos, q, v))
     if 'z' in node.attrib:
         pos = eval(node.attrib['z'], {}, vars)
-        write_to_fds(vars, "&SLCF PBZ=%e, %s %s /\n"%(pos, q, v))
+        write_to_fds("&SLCF PBZ=%e, %s %s /\n"%(pos, q, v))
     
 def paradim(node, dirlist):
     if 'name' in node.attrib:
@@ -211,7 +236,36 @@ def paradim(node, dirlist):
         
         for ip in range(np):
             dirlist[ip][node.attrib['name']] = paralist[ip]
-    
+
+def traverse(root):
+    for node in root:
+        globals()[node.tag](node)    
+ 
+def dbg(node):
+    print get_val(node, "print")
+
+def device(node):
+    check_val(node, ["q" ,"id"], req=True)
+    if check_val(node, ["x1", "x2", "y1", "y2", "z1", "z2"]):
+        x1 = get_val(node, "x1", opt=True)
+        x2 = get_val(node, "x2", opt=True)
+        y1 = get_val(node, "y1", opt=True)
+        y2 = get_val(node, "y2", opt=True)
+        z1 = get_val(node, "z1", opt=True)
+        z2 = get_val(node, "z2", opt=True)
+        q = get_val(node, "q", opt=True)
+        id = get_val(node, "id", opt=True)
+        write_to_fds("&DEVC ID='%s' XB=%f,%f,%f,%f,%f,%f QUANTITY='%s'/\n"%(id,x1,x2,y1,y2,z1,z2,q))
+        return True
+    if check_val(node, ["x", "y", "z"]):
+        x = get_val(node, "x", opt=True)
+        y = get_val(node, "y", opt=True)
+        z = get_val(node, "z", opt=True)
+        write_to_fds("&DEVC ID='%s' XYZ=%f,%f,%f QUANTITY='%s'/\n"%(id,x,y,z,q))
+        return True
+    return False
+     
+#################### MAIN LOOP
 tree = ET.parse(str(sys.argv[1]))
 root = tree.getroot()
 
@@ -223,35 +277,15 @@ for node in root:
             params[node.attrib['dim']] = []
         paradim(node, params[node.attrib['dim']])
 
-for ipd in params:
-    print "parameter dimension: %s"%ipd
-    print params[ipd]
-
-params_dim = len(params)
-
 params_id = 0
 for items in product(*[params[pd] for pd in params]):
 
-    params_id += 1
     vars = {'outfile':"output.fds", 'chid':"chid", 'title':"title", 'fds_file_open':False, 'fds_file':0, 'params_id': params_id}
+    params_id += 1
+    
     for v in items:
         vars = dict(vars.items() + v.items())
     
-    print vars 
+    traverse(root)
 
-    for node in root:
-        #print node
-    
-        if node.tag == 'obst': obst(node, vars)
-        if node.tag == 'hole': hole(node, vars)    
-        if node.tag == 'var': var(node, vars)
-        if node.tag == 'loop': loop(node, vars)
-        if node.tag == 'info': info(node, vars)
-        if node.tag == 'mesh': mesh(node, vars)
-        if node.tag == 'input': input(node, vars)
-    
-        if node.tag == 'boundary': boundary(node, vars)
-        if node.tag == 'fire': fire(node, vars)
-        if node.tag == 'slice': slice(node, vars)
-
-    close_fds_file(vars)
+    close_fds_file()
