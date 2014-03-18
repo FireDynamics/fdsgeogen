21634ef472f4b4b0710f31259853e07b7a8eefeb
Lukas Arnold <l.arnold@fz-juelich.de>
2014-03-18 17:16:38 +0100

    added slice file support

--- createfds.py
+++ createfds.py
@@ -15,6 +15,7 @@ global_args['surf'] = ['rgb', 'color', 'vel', 'hrrpua','heat_of_vaporization',
 global_args['obst'] = ['x1', 'x2', 'y1', 'y2', 'z1', 'z2', 'xb', 'surf_ids', 'surf_id', 'color']
 global_args['hole'] = ['xb']
 global_args['vent'] = ['xb', 'surf_id', 'color', 'dynamic_pressure']
+global_args['slcf'] = ['pbx', 'pby', 'pbz', 'quantity', 'vector']
 
 global_keys = {}
 global_keys['reac']  = 'REAC'
@@ -23,6 +24,7 @@ global_keys['surf']  = 'SURF'
 global_keys['obst']  = 'OBST'
 global_keys['hole']  = 'HOLE'
 global_keys['vent']  = 'VENT'
+global_keys['slcf']  = 'SLCF'
 
 def div235(n):
     r_init = int(n)
