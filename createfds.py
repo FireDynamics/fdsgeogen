b6986958af2d15b1a2806da301a7f993eb9acdd5
Andreas Meunders <andreasmeunders@gmail.com>
2015-09-14 15:50:12 +0200

    Added arguments to fds_surf

--- createfds.py
+++ createfds.py
@@ -20,7 +20,7 @@ global_args['fds_matl'] = ['specific_heat', 'conductivity', 'density', 'heat_of_
 global_args['fds_surf'] = ['rgb', 'color', 'vel', 'hrrpua', 'heat_of_vaporization',
                        'ignition_temperature', 'burn_away', 'matl_id', 'matl_mass_fraction',
                        'thickness', 'external_flux', 'backing', 'hrrupa', 'stretch_factor', 'cell_size_factor',
-                       'ramp_q', 'mlrpua', 'tmp_front', 'tmp_inner','tmp_back', 'transparency']
+                       'ramp_q', 'mlrpua', 'tmp_front', 'tmp_inner','tmp_back', 'transparency', 'net_heat_flux','emissivity']
 global_args['fds_obst'] = ['xb', 'surf_ids', 'surf_id', 'color', 'bulk_density', 'bndf_obst',
                        'thickness', 'external_flux', 'backing', 'hrrupa', 'stretch_factor', 'cell_size_factor',
                        'ramp_q', 'mlrpua']
