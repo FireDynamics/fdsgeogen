6886a280633cf9910cc20a1358fa77a58915502c
Lukas Arnold <l.arnold@fz-juelich.de>
2015-11-26 15:41:32 +0100

    added license to create script

--- scripts/fgg_create.py
+++ scripts/fgg_create.py
@@ -1,5 +1,20 @@
 #!/usr/bin/env python
 
+# This file is part of fdsgeogen.
+# 
+# fdsgeogen is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# fdsgeogen is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with fdsgeogen. If not, see <http://www.gnu.org/licenses/>.
+
 import sys
 import re
 import os
@@ -1155,4 +1170,3 @@ for items in product(*[params[pd] for pd in params]):
     dump_plot_types(plots, vars['subdir'])
 
 dump_subdirectories(subdirs)
-
