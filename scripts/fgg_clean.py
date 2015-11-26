ce27bbe4558ba12e90c75fb56a7c08a1e30f7c9d
Lukas Arnold <l.arnold@fz-juelich.de>
2015-11-26 15:41:06 +0100

    added license to clean script

--- scripts/fgg_clean.py
+++ scripts/fgg_clean.py
@@ -1,5 +1,20 @@
 #!/usr/bin/env python
 
+# This file is part of fdsgeogen.
+# 
+# fdsgeogen is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# fdsgeogen is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with fdsgeogen. If not, see <http://www.gnu.org/licenses/>.
+
 import shutil
 import os
 
@@ -25,4 +40,3 @@ if ans == 'yes':
 		shutil.rmtree(d)
 	
 	os.remove(fn_subdirlist)
-
