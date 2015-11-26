11de8d666a10dd2c1e86f6ba76b84436aaf043f7
Lukas Arnold <l.arnold@fz-juelich.de>
2015-11-26 15:40:40 +0100

    added license information for analyse script

--- scripts/fgg_analyse.py
+++ scripts/fgg_analyse.py
@@ -1,5 +1,20 @@
 #!/usr/bin/env python
 
+# This file is part of fdsgeogen.
+# 
+# fdsgeogen is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# fdsgeogen is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with fdsgeogen. If not, see <http://www.gnu.org/licenses/>.
+
 import os
 import os.path
 
@@ -231,4 +246,3 @@ for cg in global_tasks:
 		
 	if group_id != '':
 		saveDevcPlot('./', [], [0,1], [group_id], [group_q], [group_unit], cg, mode='finish')
-		
